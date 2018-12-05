2085f44cb989650142e97137b1c8693be7c933b8
winnie <wangliu023@qq.com>
2018-12-05 09:37:50 +0800

    sync

---
+++ app/src/main/java/com/winnie/utils/notifycation/MainActivity.java
@@ -0,0 +1,16 @@
+package com.winnie.utils.notifycation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author winnie
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
